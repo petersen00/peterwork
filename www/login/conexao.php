c510a43a6539a4757cf5977611a4ffebe7ebb9ca
petersen00 <51521106+petersen00@users.noreply.github.com>
2019-07-08 22:40:05 -0300

    Delete conexao.php

--- www/login/conexao.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-# Conexão com o banco de dados
-
-error_reporting(1);
-$conexao = new mysqli("mysql105.prv.f1.k8.com.br.", "peterwork", "Tp121933", "peterwork");
-$banco = mysqli_select_db($conexao,'peterwork') or die("Erro ao selecionar o banco de dados");
-?>
